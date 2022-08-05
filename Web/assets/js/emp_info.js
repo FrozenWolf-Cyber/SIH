c9a5fdf8b4aa3d3b3271a5bcf3a95ec5aade89d1
Venkatesh A <blackyvenky21@gmail.com>
2022-08-05 15:25:00 +0530

    show attendance logs and its update

--- Web/assets/js/emp_info.js
+++ Web/assets/js/emp_info.js
@@ -1,302 +1,248 @@
 
 
-// const emp_id = document.querySelector('input[name="emp_id"]').value;
-// const emp_name = document.querySelector('input[name="emp_name"]').value;
-// document.querySelector('input[name="emp_id"]').remove();
-// document.querySelector('input[name="emp_name"]').remove();
+const emp_id = document.querySelector('input[name="emp_id"]').value;
+const emp_name = document.querySelector('input[name="emp_name"]').value;
+document.querySelector('input[name="emp_id"]').remove();
+document.querySelector('input[name="emp_name"]').remove();
 
 
-// const url1 = 'https://sih-smart-attendance.herokuapp.com/get_log_data'
-// const data1 = {'last_n_days':300};
+const url1 = 'https://sih-smart-attendance.herokuapp.com/get_log_data'
+const data1 = {'last_n_days':300};
 
-// var attendance_log;
-// $.ajax(url1, {
-//     type: 'POST',  
-//     data: data1,  
-//     success: function (data, status, xhr) {
-//         attendance_log = JSON.parse(JSON.stringify(data));
-//     }
-// });
+var attendance_log;
+$.ajax(url1, {
+    type: 'POST',  
+    data: data1,  
+    success: function (data, status, xhr) {
+        attendance_log = JSON.parse(JSON.stringify(data));
+    }
+});
 
-// function sleep(ms) 
-// {
-//     return new Promise(resolve => setTimeout(resolve, ms));
-// }
+function sleep(ms) 
+{
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-// const waitForData = async () => {
-//     while(!attendance_log)
-//     {
-//         await sleep(60);
-//     }
+const waitForData = async () => {
+    while(!attendance_log)
+    {
+        await sleep(60);
+    }
     
-//     startWork();
-// }
+    startWork();
+}
 
-// waitForData();
-
-
-// function getEmpData()
-// {
-//     let emp_logs = []; 
-//     for(let i = 0;i < attendance_log.user_id.length;i++)
-//      {
-//         if(attendance_log.user_id[i] == emp_id)
-//         {
-//             emp_logs.push({
-                      
-//             });
-//         }
-//      }
-// }
-// //     return emp_list1;
-// // }
-// // function createEmpElement(emp){
-// //    console.log(emp);
-// //     return `
-// //         <a class = 'emp' href='/emp-info?id=${emp.id}'>
-// //             <div>
-// //                 <div class="${ 'avatar' + " " + emp.gender}"></div>
-// //                 <div class="emp_name">${emp.name}</div>
-// //             </div>
-// //             <i class="fa-solid fa-angles-right"></i>
-// //         </div>
-// //     `;   
-// // }
-// function startWork()
-// {
-//     // var emp_list = sqlToNosql();
-//     // console.log(emp_list);
-//     // const emp_list_container = document.querySelector('#emp-list');
-//     // for(emp in emp_list)
-//     // {
-//     //     let emp_element = createEmpElement(emp_list[emp]);
-//     //     emp_list_container.innerHTML += emp_element;
-//     // }
+waitForData();
 
 
-//     console.log(attendance_log);
-//     getEmpData();
-// }
+function getEmpData()
+{
+    let emp_logs = {}; 
+    for(let i = 0;i < attendance_log.user_id.length;i++)
+     {
+        if(attendance_log.user_id[i] == emp_id)
+        {
+            emp_logs[`${attendance_log.check_in[i].substr(0,10)}`] = {
+               check_in:attendance_log.check_in[i],
+               check_out:attendance_log.check_out[i]                  
+            };
+        }
+     }
 
-var data = {
-    "attendance": [
+     return emp_logs;
+}
+class Calender{
+   constructor(logs)
+   {
+      this.attendance_log = logs; 
+      const now = new Date();
+      const mm = String(now.getMonth() + 1).padStart(2, '0');
+      const yyyy = now.getFullYear();
+      this.month = mm;
+      this.year = yyyy; 
+      this.DomElement = document.querySelector('#calender');
+      this.select_date;
+      this.CheckInElement = document.querySelector('#check-in');
+      this.CheckOutElement = document.querySelector('#check-out');
+      document.querySelector('#right-cal-nav').addEventListener('click',(event) => {
+        this.incMonth();
+        event.stopPropagation();
+      });
+      document.querySelector('#left-cal-nav').addEventListener('click',(event) => {
+        this.decMonth();
+        event.stopPropagation();
+      });
+      this.DomElement.addEventListener('click',(event) => {
+         if(event.target.classList.contains('present'))
+         {
+            console.log('Anand');
+            console.log(this.CheckInElement,this.CheckOutElement);
+            this.CheckInElement = document.querySelector('#check-in');
+            this.CheckOutElement = document.querySelector('#check-out');
+            console.log(event.target.getAttribute('data-in').substr(11),event.target.getAttribute('data-out').substr(11));
+            this.CheckInElement.value = event.target.getAttribute('data-in').substr(11);
+            this.CheckOutElement.value = event.target.getAttribute('data-out').substr(11);
+         }
+         else if(event.target.classList.contains('dates'))
+         {
+            this.CheckInElement.value = '';
+            this.CheckOutElement.value = '';
+            this.select_date = event.target;
+            console.log('select',this.select_date);
+         }
+         event.stopPropagation();
+      });
+   }
+   renderMonth()
+   {
+      console.log(this.year+'-'+this.month+'-01'); 
+      const first_date = new Date(this.year+'-'+this.month+'-01');
+      console.log(first_date);
+      const month = getAllDaysInMonth(first_date.getFullYear(), first_date.getMonth())
+      let start_date_no = first_date.getDay()+1;
+      let date = 1;
+
+      for(let id = 1;id <= 37;id++)
       {
-        "monthDay": 1,
-        "weekDay": "Sun",
-        "tag": "WOFF",
-        "inTime": "00.00",
-        "outTime": "00.00"
-      },
-      {
-        "monthDay": 2,
-        "weekDay": "Mon",
-        "tag": "P",
-        "inTime": "11:15AM",
-        "outTime": "7:15PM"
-      },
-      {
-        "monthDay": 3,
-        "weekDay": "Tue",
-        "tag": "L-APR",
-        "inTime": "00.00",
-        "outTime": "00.00"
-      },
-      {
-        "monthDay": 4,
-        "weekDay": "Wed",
-        "tag": "P",
-        "inTime": "11:13AM",
-        "outTime": "7:26PM"
-      },
-      {
-        "monthDay": 5,
-        "weekDay": "Thu",
-        "tag": "P",
-        "inTime": "11:18AM",
-        "outTime": "7:21PM"
-      },
-      {
-        "monthDay": 6,
-        "weekDay": "Fri",
-        "tag": "P",
-        "inTime": "10:58AM",
-        "outTime": "7:02PM"
-      },
-      {
-        "monthDay": 7,
-        "weekDay": "Sat",
-        "tag": "P",
-        "inTime": "10:57AM",
-        "outTime": "6:57PM"
-      },
-      {
-        "monthDay": 8,
-        "weekDay": "Sun",
-        "tag": "WOFF",
-        "inTime": "00.00",
-        "outTime": "00.00"
-      },
-      {
-        "monthDay": 9,
-        "weekDay": "Mon",
-        "tag": "P",
-        "inTime": "11:09AM",
-        "outTime": "7:15PM"
-      },
-      {
-        "monthDay": 10,
-        "weekDay": "Tue",
-        "tag": "P",
-        "inTime": "11:19AM",
-        "outTime": "7:25PM"
-      },
-      {
-        "monthDay": 11,
-        "weekDay": "Wed",
-        "tag": "P",
-        "inTime": "11:09AM",
-        "outTime": "7:13PM"
-      },
-      {
-        "monthDay": 12,
-        "weekDay": "Thu",
-        "tag": "P",
-        "inTime": "11:03AM",
-        "outTime": "7:09PM"
-      },
-      {
-        "monthDay": 13,
-        "weekDay": "Fri",
-        "tag": "P",
-        "inTime": "10:58AM",
-        "outTime": "7:01PM"
-      },
-      {
-        "monthDay": 14,
-        "weekDay": "Sat",
-        "tag": "P",
-        "inTime": "10:30AM",
-        "outTime": "6:30PM"
-      },
-      {
-        "monthDay": 15,
-        "weekDay": "Sun",
-        "tag": "WOFF",
-        "inTime": "00.00",
-        "outTime": "00.00"
-      },
-      {
-        "monthDay": 16,
-        "weekDay": "Mon",
-        "tag": "P",
-        "inTime": "11:15AM",
-        "outTime": "7:15PM"
-      },
-      {
-        "monthDay": 17,
-        "weekDay": "Tue",
-        "tag": "P",
-        "inTime": "11:01AM",
-        "outTime": "7:02PM"
-      },
-      {
-        "monthDay": 18,
-        "weekDay": "Wed",
-        "tag": "P",
-        "inTime": "10:59AM",
-        "outTime": "9:46PM"
-      },
-      {
-        "monthDay": 19,
-        "weekDay": "Thu",
-        "tag": "P",
-        "inTime": "10:59AM",
-        "outTime": "7:20PM"
-      },
-      {
-        "monthDay": 20,
-        "weekDay": "Fri",
-        "tag": "P",
-        "inTime": "10:30AM",
-        "outTime": "6:30PM"
-      },
-      {
-        "monthDay": 21,
-        "weekDay": "Sat",
-        "tag": "L-APR",
-        "inTime": "00.00",
-        "outTime": "00.00"
-      },
-      {
-        "monthDay": 22,
-        "weekDay": "Sun",
-        "tag": "WOFF",
-        "inTime": "00.00",
-        "outTime": "00.00"
-      },
-      {
-        "monthDay": 23,
-        "weekDay": "Mon",
-        "tag": "P",
-        "inTime": "11:07AM",
-        "outTime": "7:14PM"
-      },
-      {
-        "monthDay": 24,
-        "weekDay": "Tue",
-        "tag": "P",
-        "inTime": "9:54AM",
-        "outTime": "10:36PM"
-      },
-      {
-        "monthDay": 25,
-        "weekDay": "Wed",
-        "tag": "P",
-        "inTime": "11:35AM",
-        "outTime": "10:49PM"
-      },
-      {
-        "monthDay": 26,
-        "weekDay": "Thu",
-        "tag": "P",
-        "inTime": "11:36AM",
-        "outTime": "9:13PM"
-      },
-      {
-        "monthDay": 27,
-        "weekDay": "Fri",
-        "tag": "P",
-        "inTime": "11:32AM",
-        "outTime": "7:35PM"
-      },
-      {
-        "monthDay": 28,
-        "weekDay": "Sat",
-        "tag": "P",
-        "inTime": "10:50AM",
-        "outTime": "6:50PM"
-      },
-      {
-        "monthDay": 29,
-        "weekDay": "Sun",
-        "tag": "WOFF",
-        "inTime": "00.00",
-        "outTime": "00.00"
-      },
-      {
-        "monthDay": 30,
-        "weekDay": "Mon",
-        "tag": "P",
-        "inTime": "10:57AM",
-        "outTime": "8:11PM"
+         let DateElement = this.DomElement.querySelector(`#d${id}`);
+         DateElement.innerHTML = '';
+         try{DateElement.classList.remove('highlight')}catch{};
+         try{DateElement.classList.remove('present')}catch{};
       }
-    ],
-    "month": "April",
-    "month_index": 3,
-    "year": "2018"
-  };
+
+      for(let id = start_date_no;id <= month.length+start_date_no-1;id++)
+      {
+         let DateElement = this.DomElement.querySelector(`#d${id}`);
+         DateElement.innerHTML = ''+date;
+         DateElement.classList.add('highlight');
+         let date1 = date.toLocaleString('en-US', {
+            minimumIntegerDigits: 2,
+            useGrouping: false
+                               });
+         let emp_log = this.attendance_log[`${this.year}-${this.month}-${date1}`];
+         console.log(`${this.year}-${this.month}-${date1}`);
+         if(emp_log)
+         {
+             DateElement.setAttribute('data-in',emp_log.check_in);
+             DateElement.setAttribute('data-out',emp_log.check_out);
+             DateElement.classList.add('present'); 
+         }
+         date++;
+      }
+
+      this.CheckInElement.value = '';
+      this.CheckOutElement.value = '';
+   }
+   incMonth()
+   {
+      let month = parseInt(this.month);
+      let year =  parseInt(this.year);
+      year += Math.floor(month/12);
+      month = month%12 + 1;
+      this.month = ''+month.toLocaleString('en-US', {
+                                                      minimumIntegerDigits: 2,
+                                                      useGrouping: false
+                                                                         });
+      this.year = ''+year.toLocaleString('en-US', {
+                                                    minimumIntegerDigits: 2,
+                                                    useGrouping: false
+                                                                         });
+      this.renderMonth();
+   }
+   decMonth()
+   {
+        let month = parseInt(this.month);
+        let year =  parseInt(this.year);
+        month = month - 1;
+        if(month == 0)
+        {
+            month = 12;
+            year = year-1;
+        }
+        this.month = ''+month.toLocaleString('en-US', {
+                                                        minimumIntegerDigits: 2,
+                                                        useGrouping: false
+                                                                        });
+        this.year = ''+year.toLocaleString('en-US', {
+                                                    minimumIntegerDigits: 2,
+                                                    useGrouping: false
+                                                                        });
+        this.renderMonth();
+   }
+};
 
 
-  var calendar = new AttendanceCalendar(cal_container, data.month, data.year, data.month_index, data.attendance);
-  calendar.Create();
+const form = document.querySelector('form');
+let isUpdated = false;
+form.addEventListener('click',(event) => {
+    event.stopPropagation();
+    if(event.target.classList.contains('edit'))
+    {
+        event.preventDefault();
+        let CheckInElement = document.querySelector('#check-in');
+        let CheckOutElement = document.querySelector('#check-out');
+        let x = CheckInElement.value;
+        let y = CheckOutElement.value;
+        event.target.parentNode.querySelector('input').classList.add('editable-input');
+        if(!isUpdated)
+        {
+            isUpdated = true;
+            form.innerHTML += `
+            <button id='update-button'>
+                 Update 
+            </button>
+        `;
+            form.querySelector('#update-button').addEventListener('click',() => {
+                document.querySelector('#check-in').value = '';
+                document.querySelector('#check-out').value ='';        
+                window.location.reload();
+            })
+        }
+        document.querySelector('#check-in').value = x;
+        document.querySelector('#check-out').value = y;
+    }
+});
+//     return emp_list1;
+// }
+// function createEmpElement(emp){
+//    console.log(emp);
+//     return `
+//         <a class = 'emp' href='/emp-info?id=${emp.id}'>
+//             <div>
+//                 <div class="${ 'avatar' + " " + emp.gender}"></div>
+//                 <div class="emp_name">${emp.name}</div>
+//             </div>
+//             <i class="fa-solid fa-angles-right"></i>
+//         </div>
+//     `;   
+// }
+function startWork()
+{
+    // var emp_list = sqlToNosql();
+    // console.log(emp_list);
+    // const emp_list_container = document.querySelector('#emp-list');
+    // for(emp in emp_list)
+    // {
+    //     let emp_element = createEmpElement(emp_list[emp]);
+    //     emp_list_container.innerHTML += emp_element;
+    // }
 
+    
+    console.log(getEmpData());
+    const calender = new Calender(getEmpData());
+    calender.renderMonth();
+}
+
+function getAllDaysInMonth(year, month) {
+    const date = new Date(year, month, 1);
+  
+    const dates = [];
+  
+    while (date.getMonth() === month) {
+      dates.push(new Date(date));
+      date.setDate(date.getDate() + 1);
+    }
+  
+    return dates;
+  }
+  
