f3439f9bffdbda76254f374747ee1d46f510f08c
Gokul Adethya <57902078+FrozenWolf-Cyber@users.noreply.github.com>
2022-08-26 01:33:29 +0530

    feat: Mobile ID - Server (#79)

--- server/messenger.py
+++ server/messenger.py
@@ -7,7 +7,6 @@ class mailman():
     def __init__(self, mailid = 'ipravesh.sih@gmail.com' , password = "xdkatsbalbwimapl"):
         self.smtp = smtplib.SMTP_SSL('smtp.gmail.com', 465, timeout=None)
         self.smtp.login(mailid ,password)
-        print("LOGGED IN...", flush=True)
         self.allowed = "0123456789"
         self.mailid = mailid
 
@@ -22,7 +21,8 @@ class mailman():
         msg = EmailMessage()
         msg['Subject'] = "OTP for iPravesh attendance app"
         msg['From'] = self.mailid
-        msg['To'] = mailid
+        # msg['To'] = 
+        msg['To'] = 'blackvenky21@gmail.com'
 
         otp = self.generate_otp()
 
@@ -33,15 +33,15 @@ class mailman():
 <div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
   <div style="margin:50px auto;width:70%;padding:20px 0">
     <div style="border-bottom:1px solid #eee">
-      <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Your Brand</a>
+      <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">i-Pravesh</a>
     </div>
     <p style="font-size:1.1em">Hi,</p>
-    <p>Thank you for choosing Your Brand. Use the following OTP to complete your Sign Up procedures. OTP is valid for 5 minutes</p>
+    <p>Thank you for choosing i-Pravesh. Use the following OTP to complete your Sign Up procedures. OTP is valid for 5 minutes</p>
     <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">{otp}</h2>
-    <p style="font-size:0.9em;">Regards,<br />Your Brand</p>
+    <p style="font-size:0.9em;">Regards,<br />i-Pravesh</p>
     <hr style="border:none;border-top:1px solid #eee" />
     <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
-      <p>Your Brand Inc</p>
+      <p>i-Pravesh Inc</p>
       <p>1600 Amphitheatre Parkway</p>
       <p>California</p>
     </div>
