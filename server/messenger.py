622aecbffc73d338396b1316999e980e698817c1
FrozenWolf-Cyber <frozenwolf2003.com@gmail.com>
2022-08-26 14:27:04 +0530

    mass reg

--- server/messenger.py
+++ server/messenger.py
@@ -7,6 +7,7 @@ class mailman():
     def __init__(self, mailid = 'ipravesh.sih@gmail.com' , password = "xdkatsbalbwimapl"):
         self.smtp = smtplib.SMTP_SSL('smtp.gmail.com', 465, timeout=None)
         self.smtp.login(mailid ,password)
+        print("LOGGED IN...", flush=True)
         self.allowed = "0123456789"
         self.mailid = mailid
 
@@ -17,12 +18,12 @@ class mailman():
 
         return OTP
 
-    def send_otp(self, mailid):
+    def send_otp(self, emp_no, mailid):
         msg = EmailMessage()
         msg['Subject'] = "OTP for iPravesh attendance app"
         msg['From'] = self.mailid
-        # msg['To'] = 
-        msg['To'] = 'blackvenky21@gmail.com'
+        # msg['To'] = 'blackyvenky21@gmail.com'
+        msg['To'] = mailid
 
         otp = self.generate_otp()
 
@@ -31,12 +32,12 @@ class mailman():
 <html>
     <body>
 <div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
-  <div style="margin:50px auto;width:70%;padding:20px 0">
+  <div style="margin:50px auto;width:70%;padding:20px 0"> 
     <div style="border-bottom:1px solid #eee">
       <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">i-Pravesh</a>
     </div>
     <p style="font-size:1.1em">Hi,</p>
-    <p>Thank you for choosing i-Pravesh. Use the following OTP to complete your Sign Up procedures. OTP is valid for 5 minutes</p>
+    <p>Thank you for choosing i-Pravesh. Use the following OTP to complete Sign Up procedures for employee number {emp_no} with {mailid} as mailid. OTP is valid for 5 minutes</p>
     <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">{otp}</h2>
     <p style="font-size:0.9em;">Regards,<br />i-Pravesh</p>
     <hr style="border:none;border-top:1px solid #eee" />
